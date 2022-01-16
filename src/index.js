7235707505b51dafc079d7ff35f12360535c94a5
Jorge A. Macias <jmacias3118@jal.conalep.edu.mx>
2022-01-16 00:10:17 -0600

    Feat - Adding middlewares and main route
    I add the main route for the app, also I add middlewares that help me
    with the static content.
    
    by last, I set the view engine to ejs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const colors = require('colors')
+const path = require('path')
 
 const app = express();
 
+app.use('/public', express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname + '/views'))
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world from express!</h1>')
+    res.render('index')
 });
 
 app.listen(80, () => {
-    console.log("Server on!");
+    console.log("Backend on!".green.inverse);
 })
